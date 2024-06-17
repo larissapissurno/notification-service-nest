32b49cca4e514114fdf22b2c376b17118db5abcc
Larissa Pissurno <larissapissurno@gmail.com>
2024-06-17 18:30:48 -0300

    Apply mapper concept

--- src/infra/database/prisma/repositories/prisma-notifications.repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications.repository.ts
@@ -2,22 +2,16 @@ import { Notification } from 'src/application/entities/notification';
 import { NotificationsRepository } from 'src/application/repositories/notifications-repository';
 import { PrismaService } from '../prisma.service';
 import { Injectable } from '@nestjs/common';
+import { PrismaNotificationMapper } from '../mappers/prisma-notification-mapper';
 
 @Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
   constructor(private readonly prismaService: PrismaService) {}
 
   async create(notification: Notification): Promise<void> {
-    // Implementation here
+    const raw = PrismaNotificationMapper.toPrisma(notification);
     await this.prismaService.notification.create({
-      data: {
-        id: notification.id,
-        content: notification.content.value,
-        category: notification.category,
-        recipientId: notification.recipientId,
-        readAt: notification.readAt,
-        createdAt: notification.createdAt,
-      },
+      data: raw,
     });
   }
 }
