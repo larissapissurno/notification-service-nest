7ec10e06d5bd80ca2a439705c7963db5b8e09fda
Larissa Pissurno <larissapissurno@gmail.com>
2024-06-17 21:15:19 -0300

    Refactor notification constructor and add repository methods

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,6 @@
 import { Notification } from '@/application/entities/notification';
+import { NotificationContent } from '@/application/entities/notification-content';
+import { Notification as RawNotification } from '@prisma/client';
 
 export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
@@ -10,5 +12,19 @@ export class PrismaNotificationMapper {
       readAt: notification.readAt,
       createdAt: notification.createdAt,
     };
+  }
+
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        content: new NotificationContent(raw.content),
+        category: raw.category,
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        canceledAt: raw.canceledAt,
+        createdAt: raw.createdAt,
+      },
+      raw.id,
+    );
   }
 }
--- src/infra/database/prisma/repositories/prisma-notifications.repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications.repository.ts
@@ -9,6 +9,22 @@ export class PrismaNotificationsRepository implements NotificationsRepository {
   constructor(private readonly prismaService: PrismaService) {}
 
   async findById(notificationId: string): Promise<Notification | null> {
+    const notification = await this.prismaService.notification.findUnique({
+      where: {
+        id: notificationId,
+      },
+    });
+
+    return notification
+      ? PrismaNotificationMapper.toDomain(notification)
+      : null;
+  }
+
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    throw new Error('Method not implemented.');
+  }
+
+  async countManyByRecipientId(recipientId: string): Promise<number> {
     throw new Error('Method not implemented.');
   }
 
